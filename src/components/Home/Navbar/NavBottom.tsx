1d0d6baa75f0b808eec8948636b9703dfd181e6b
siddharth <siddharthray47@gmail.com>
2024-11-08 18:00:05 +0530

    navbuttonissuedone

--- src/components/Home/Navbar/NavBottom.tsx
+++ src/components/Home/Navbar/NavBottom.tsx
@@ -5,16 +5,14 @@ const NavBottom = () => {
   return (
     <div className="bg-primary">
       <div className="container  hidden lg:flex justify-evenly text-secondary py-2 font-semibold">
-        <h3>Home</h3>
-        <h3>About </h3>
-        <h3>List Categories</h3>
-        <h3>Best Seller</h3>
-        <h3>Shop</h3>
-        <h3>Blog</h3>
-        <h3>Contact</h3>
-        <Link href={"/brands"}>
-          <h3>Shop by Brands</h3>
-        </Link>
+        <Link href={"/"}>Home</Link>
+        <Link href={""}>About </Link>
+        <Link href={"/categories"}>List Categories</Link>
+        <Link href={""}>Best Seller</Link>
+        <Link href={""}>Shop</Link>
+        <Link href={""}>Blog</Link>
+        <Link href={"/contact"}>Contact</Link>
+        <Link href={"/brands"}>Shop by Brands</Link>
       </div>
     </div>
   );
