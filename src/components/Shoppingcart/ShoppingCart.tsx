5c0bf69a2e7b14e6e1efaa188d911c4f0bedf352
siddharth <siddharthray47@gmail.com>
2024-11-09 15:04:36 +0530

    cart

--- src/components/Shoppingcart/ShoppingCart.tsx
+++ src/components/Shoppingcart/ShoppingCart.tsx
@@ -9,87 +9,84 @@ const ShoppingCart = () => {
   return (
     <>
       <CommonBanner icon={svgIconBannerHome} path={["Cart"]} />
-      <div className="flex lg:justify-center lg:flex-row flex-col  gap-5 mt-5 mb-5">
-        <div>
-          <div className="text-[30px] font-bold border-[#E4E7E9] border-l border-r border-t pl-3 h-[64px] flex justify-start items-center">
+      <div className="flex lg:justify-center md:flex-row flex-col  lg:items-start gap-5 mt-5 mb-5">
+        <div className="">
+          <div className="text-[30px] border-[#E4E7E9] border-r border-l border-t font-bold  pl-3 h-[64px] flex justify-start items-center">
             Shopping Cart
           </div>
 
-          <table className="border-[#E4E7E9] border w-[800px]">
-            <thead>
-              <tr className="bg-[#F2F4F5] h-[38px]">
-                <th className="text-[#475156] font-medium text-left pl-3">
-                  Products
-                </th>
-                <th className="text-[#475156] font-medium">Price</th>
-                <th className="text-[#475156] font-medium">Quantity</th>
-                <th className="text-[#475156] font-medium">Sub-Total</th>
-              </tr>
-            </thead>
-            <tbody className="h-[70px]">
-              <tr>
-                <td>
-                  <div className="flex items-center gap-2 h-[70px] pl-2">
-                    <CircleX color="#929FA5" />
-                    <Image src={cpu} alt="product" className="w-[50px] " />
-                    <div>TIPASON – Gaming Desktop – AMD 3000G</div>
-                  </div>
-                </td>
-                <td className="text-center">
-                  <span className="text-[#929FA5] line-through">
-                    &nbsp;<span className="">$199</span>
-                  </span>
-                  $70
-                </td>
-                <td className="flex justify-center items-center h-[70px]">
-                  <div className="border border-[#E4E7E9] rounded w-[148px] h-[48px] flex justify-center items-center gap-4">
-                    <span className="cursor-pointer">
-                      <Minus />
-                    </span>
-                    <span className="text-[18px]">23</span>
-                    <span className="cursor-pointer">
-                      <Plus />
-                    </span>
-                  </div>
-                </td>
-                <td className="text-center">$70</td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="flex items-center gap-2 h-[70px] pl-2">
-                    <CircleX color="#EE5858" />
-                    <Image src={cpu} alt="product" className="w-[50px] " />
-                    <div>TIPASON – Gaming Desktop – AMD 3000G</div>
-                  </div>
-                </td>
-                <td className="text-center">
-                  <span className="text-[#929FA5] line-through">
-                    &nbsp;<span className="">$199</span>
-                  </span>
-                  $70
-                </td>
-                <td className="flex justify-center items-center h-[70px]">
-                  <div className="border border-[#E4E7E9] rounded w-[148px] h-[48px] flex justify-center items-center gap-4">
-                    <span className="cursor-pointer">
-                      <Minus />
-                    </span>
-                    <span className="text-[18px]">23</span>
-                    <span className="cursor-pointer">
-                      <Plus />
-                    </span>
-                  </div>
-                </td>
-                <td className="text-center">$70</td>
-              </tr>
-            </tbody>
-          </table>
-          <div className="w-[800px] border-[#E4E7E9] border-l border-r border-b  flex justify-between p-4  ">
-            <button className="text-[#EB4227] border border-[#EB4227] font-bold w-[200px] h-[48px] flex justify-center items-center gap-2 ">
-              {" "}
+          <div className="border-[#E4E7E9] border-t border-b lg:border-l lg:border-r h-[38px]  bg-[#F2F4F5] flex items-center ">
+            <div className="text-[#475156] font-medium w-[350px] pl-4">
+              Products
+            </div>
+            <div className="text-[#475156] font-medium w-[100px] text-center">
+              Price
+            </div>
+            <div className="text-[#475156] font-medium w-[200px] text-center">
+              Quantity
+            </div>
+            <div className="text-[#475156] font-medium w-[150px] text-center">
+              Sub-totals
+            </div>
+          </div>
+
+          <div className="flex items-center  border-[#E4E7E9] lg:border-r lg:border-l border-b">
+            <div className="flex items-center gap-2 h-[70px] pl-2 w-[350px]">
+              <CircleX color="#929FA5" />
+              <Image src={cpu} alt="product" className="w-[50px] " />
+              <div className="text-sm">TIPASON – Gaming Desktop – AMD 3000G</div>
+            </div>
+            <span className="w-[100px] text-center">
+              <span className="text-[#929FA5] line-through">
+                &nbsp;<span className="">$199</span>
+              </span>
+              $70
+            </span>
+            <div className="lg:w-[200px] w-[80px]  flex justify-center">
+              <div className="border border-[#E4E7E9] rounded w-[148px] h-[48px] flex justify-center items-center gap-4">
+                <span className="cursor-pointer">
+                  <Minus />
+                </span>
+                <span className="text-[18px]">23</span>
+                <span className="cursor-pointer">
+                  <Plus />
+                </span>
+              </div>
+            </div>
+            <span className="w-[150px] text-center">$70</span>
+          </div>
+          {/* <div className="flex  items-center border-r border-l border-b">
+            <div className="flex items-center gap-2 h-[70px] pl-2 w-[350px]">
+              <CircleX color="#EB4227" />
+              <Image src={cpu} alt="product" className="w-[50px] " />
+              <div>TIPASON – Gaming Desktop – AMD 3000G</div>
+            </div>
+            <span className="w-[100px] text-center">
+              <span className="text-[#929FA5] line-through">
+                &nbsp;<span className="">$199</span>
+              </span>
+              $70
+            </span>
+            <div className="w-[200px] flex justify-center">
+              <div className="border border-[#E4E7E9] rounded w-[148px] h-[48px] flex justify-center items-center gap-4">
+                <span className="cursor-pointer">
+                  <Minus />
+                </span>
+                <span className="text-[18px]">23</span>
+                <span className="cursor-pointer">
+                  <Plus />
+                </span>
+              </div>
+            </div>
+            <span className="w-[150px] text-center">$70</span>
+          </div> */}
+
+          <div className="flex justify-between p-4  border-r border-l border-b">
+            <button className="text-[#EB4227] border border-[#EB4227] font-bold lg:w-[200px] w-[120px] text-[10px] lg:text-[18px] h-[48px] flex justify-center items-center gap-2 ">
               <ArrowLeft />
               Continue Shopping
             </button>
-            <button className="text-[#EB4227] border border-[#EB4227] font-bold w-[149px] h-[48px]  ">
+            <button className="text-[#EB4227] border border-[#EB4227] font-bold lg:w-[149px] w-[80px] text-[10px] lg:text-[18px] h-[48px]  ">
               Update cart
             </button>
           </div>
