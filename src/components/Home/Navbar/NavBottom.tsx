0021902e293baad42c72a01d5af4bf086c0c0260
siddharth <siddharthray47@gmail.com>
2024-11-12 22:35:53 +0530

    dropdown

--- src/components/Home/Navbar/NavBottom.tsx
+++ src/components/Home/Navbar/NavBottom.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import { ChevronDown, ChevronRight } from "lucide-react";
 
 const NavBottom = () => {
   return (
@@ -7,7 +8,105 @@ const NavBottom = () => {
       <div className="container  hidden lg:flex justify-evenly text-secondary py-2 font-semibold">
         <Link href={"/"}>Home</Link>
         <Link href={""}>About </Link>
-        <Link href={"/categories"}>List Categories</Link>
+        <div className="relative group z-50">
+          <div className="cursor-pointer flex items-center">
+            List Categories <ChevronDown />
+          </div>
+          <div className="absolute hidden p-5 w-[1000px] h-[500px] group-hover:flex flex-col bg-white text-primary shadow-lg  mt-2 rounded-md">
+            <div className="flex gap-5">
+              <div className="w-[200px] h-[400px]">
+                <div className="border border-[#E5E6E8] w-[200px] h-[64px] flex justify-center items-center">
+                  Computer Accessories <ChevronRight />
+                </div>
+                <div className="border border-[#E5E6E8] w-[200px] h-[64px] flex justify-center items-center">
+                  Adapters <ChevronRight />
+                </div>
+                <div className="border border-[#E5E6E8] w-[200px] h-[64px] flex justify-center items-center">
+                  Batteries <ChevronRight />
+                </div>
+                <div className="border border-[#E5E6E8] w-[200px] h-[64px] flex justify-center items-center">
+                  Cables <ChevronRight />
+                </div>
+                <div className="border border-[#E5E6E8] w-[200px] h-[64px] flex justify-center items-center">
+                  Laptop <ChevronRight />
+                </div>
+                <div className="border border-[#E5E6E8] w-[200px] h-[64px] flex justify-center items-center">
+                  Cameras <ChevronRight />
+                </div>
+                <div className="border border-[#E5E6E8] w-[200px] h-[64px] flex justify-center items-center">
+                  Lighting <ChevronRight />
+                </div>
+              </div>
+              <div className=" w-[800px] h-[400px] flex flex-wrap gap-5">
+                <div className="flex flex-col ">
+                  <span>Computer Accessories</span>
+                  <span className="text-black">Cables</span>
+                  <span className="text-black">Consumables</span>
+                  <span className="text-black">Gaming Products</span>
+                  <span className="text-black">Point of Sale (Pos)</span>
+                </div>
+                <div className="flex flex-col ">
+                  <span>Computer Accessories</span>
+                  <span className="text-black">Cables</span>
+                  <span className="text-black">Consumables</span>
+                  <span className="text-black">Gaming Products</span>
+                  <span className="text-black">Point of Sale (Pos)</span>
+                </div>
+                <div className="flex flex-col ">
+                  <span>Computer Accessories</span>
+                  <span className="text-black">Cables</span>
+                  <span className="text-black">Consumables</span>
+                  <span className="text-black">Gaming Products</span>
+                  <span className="text-black">Point of Sale (Pos)</span>
+                </div>
+                <div className="flex flex-col ">
+                  <span>Computer Accessories</span>
+                  <span className="text-black">Cables</span>
+                  <span className="text-black">Consumables</span>
+                  <span className="text-black">Gaming Products</span>
+                  <span className="text-black">Point of Sale (Pos)</span>
+                </div>
+                <div className="flex flex-col ">
+                  <span>Computer Accessories</span>
+                  <span className="text-black">Cables</span>
+                  <span className="text-black">Consumables</span>
+                  <span className="text-black">Gaming Products</span>
+                  <span className="text-black">Point of Sale (Pos)</span>
+                </div>
+                <div className="flex flex-col ">
+                  <span>Computer Accessories</span>
+                  <span className="text-black">Cables</span>
+                  <span className="text-black">Consumables</span>
+                  <span className="text-black">Gaming Products</span>
+                  <span className="text-black">Point of Sale (Pos)</span>
+                </div>
+                <div className="flex flex-col ">
+                  <span>Computer Accessories</span>
+                  <span className="text-black">Cables</span>
+                  <span className="text-black">Consumables</span>
+                  <span className="text-black">Gaming Products</span>
+                  <span className="text-black">Point of Sale (Pos)</span>
+                </div>
+                <div className="flex flex-col ">
+                  <span>Computer Accessories</span>
+                  <span className="text-black">Cables</span>
+                  <span className="text-black">Consumables</span>
+                  <span className="text-black">Gaming Products</span>
+                  <span className="text-black">Point of Sale (Pos)</span>
+                </div>
+
+                 <div className="flex flex-col ">
+                  <span>Computer Accessories</span>
+                  <span className="text-black">Cables</span>
+                  <span className="text-black">Consumables</span>
+                  <span className="text-black">Gaming Products</span>
+                  <span className="text-black">Point of Sale (Pos)</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+       
         <Link href={""}>Best Seller</Link>
         <Link href={""}>Shop</Link>
         <Link href={""}>Blog</Link>
