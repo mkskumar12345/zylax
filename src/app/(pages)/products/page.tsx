0fddcedfc38442cf4ba8d639d0fbc71838d05985
siddharth <siddharthray47@gmail.com>
2024-11-23 15:02:52 +0530

    brands in container

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -1,11 +1,13 @@
 import Products from "@/app/_components/Products/Products";
 import React from "react";
+
+
 interface URLSearchParams {
   brand: string | number | undefined;
 }
 const page = ({ searchParams }: { searchParams: URLSearchParams }) => {
   const brand = searchParams?.brand;
-  return <Products brand={brand} />;
+  return <Products brand={brand}  />;
 };
 
 export default page;
