bda978cd073c548a68f9c3f9a74a4385621d0d7f
Ankush Agrawal <codecanotechnology@gmail.com>
2024-11-29 17:45:54 +0530

    component created

--- src/app/(pages)/category/[category]/page.tsx
+++ src/app/(pages)/category/[category]/page.tsx
@@ -1,13 +1,12 @@
-import Products from "@/app/_components/Products/Products";
+import Category from "@/app/_components/Category/Category";
 import React from "react";
-
 
 interface URLSearchParams {
   brand: string | number | undefined;
 }
 const page = ({ searchParams }: { searchParams: URLSearchParams }) => {
   const brand = searchParams?.brand;
-  return <Products brand={brand}  />;
+  return <Category brand={brand} />;
 };
 
 export default page;
