005bd1e0111c69d5b67fd8ba3a264782a4d99761
mukesh <mukesh.durapid@gmail.com>
2024-11-29 09:52:04 +0530

    updated footer

--- src/components/Home/Footer/FooterBottom.tsx
+++ src/components/Home/Footer/FooterBottom.tsx
@@ -9,19 +9,20 @@ import Image from "next/image";
 import React from "react";
 
 const FooterBottom = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <div className="bg-[#151515]">
       <div className="md:container p-3 ">
         <div className="flex gap-3 lg:flex-nowrap flex-wrap justify-between md:h-[93px] items-center">
           <div className="text-sm flex gap-2 text-white font-regular">
-            © 2024 <h4 className="font-bold">Zylax</h4> . All Rights Reserved
+            © {currentYear} <h4 className="font-bold">Zylax</h4> . All Rights Reserved
           </div>
           <div className="flex flex-wrap md:flex-nowrap gap-4">
             <Image src={pngPaypal} alt="paypal" />
             <Image src={pngRupay} alt="paypal" />
-            <Image src={pngVisa} alt="paypal" />
+            {/* <Image src={pngVisa} alt="paypal" /> */}
             <Image src={pngStripe} alt="paypal" />
-            <Image src={pngKlarna} alt="paypal" />
+            {/* <Image src={pngKlarna} alt="paypal" /> */}
           </div>
         </div>
       </div>
