e565001e8c88f9320e9c764c4ec621e75ed79450
AjayAtEoxysIT <76676650+Akpanwal2001@users.noreply.github.com>
2024-12-08 00:21:39 +0530

    categories

--- src/components/Popup/CategoriesPopup/Categories.tsx
+++ src/components/Popup/CategoriesPopup/Categories.tsx
@@ -14,8 +14,14 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import allPagesRoutes from "@/constants/allPagesRoutes";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { DialogTitle } from "@radix-ui/react-dialog";
 
 const Categories = () => {
+  const [breadCrumps, setBreadCrumps] = useState<any[]>([
+    {
+      title: "Categories",
+    },
+  ]);
   const router = useRouter();
   const [categoriesChilds, setCategoriesChilds] = useState<any[]>([]);
   const isPopupOpen = useSelector((state: RootState) => state.popups.ISOPEN);
@@ -26,11 +32,18 @@ const Categories = () => {
   const onCategoryClick = (item: any) => {
     if (item?.child?.length) {
       setCategoriesChilds(item?.child);
+      setBreadCrumps((prev) => [...prev, { title: item?.title }]);
     } else {
       router.push(allPagesRoutes?.PRODUCTS);
       dispatch(TOGGLE(popupTypes.CLOSE));
     }
   };
+
+  const onClickBreadCrump = (item: any) => {
+    router.push(item?.path);
+    dispatch(TOGGLE(popupTypes.CLOSE));
+  };
+
   return (
     <Dialog
       open={isPopupOpen === popupTypes.CATEGORIES}
@@ -57,8 +70,15 @@ const Categories = () => {
           >
             <X size={28} strokeWidth={2} />
           </DialogClose>
+          <DialogTitle className="font-semibold flex -mt-3 mb-2">
+            {breadCrumps?.map((item: any, index: number) => (
+              <span className="cursor-pointer" key={`breadcrupm-${item?.id}`}>
+                {item?.title} {index === breadCrumps?.length - 1 ? null : "/ "}
+              </span>
+            ))}
+          </DialogTitle>
           <div className="grid h-full lg:grid-cols-4 md:grid-cols-3 grid-cols-2  gap-5">
-            <ScrollArea className="h-[400px]  w-full  rounded-md border">
+            <ScrollArea className="max-h-[400px]  w-full  rounded-md flex flex-col gap-1">
               {categories?.data?.map(
                 (item: {
                   title: string;
@@ -68,7 +88,7 @@ const Categories = () => {
                 }) => (
                   <div
                     onClick={() => onCategoryClick(item)}
-                    className="border group flex justify-between   border-[#E5E6E8] px-4 py-2 cursor-pointer "
+                    className="border group flex justify-between   border-[#f2f3f4] px-4 py-2 cursor-pointer "
                   >
                     <span
                       className="!visible"
@@ -81,13 +101,13 @@ const Categories = () => {
                         {item?.description}
                       </div>
                     </div>
-                    {item?.child?.length > 0 && <ChevronRight />}
+                    {item?.child?.length > 0 && <ChevronRight className="" />}
                   </div>
                 )
               )}
             </ScrollArea>
             {categoriesChilds?.length > 0 && (
-              <ScrollArea className=" md:col-span-2 sm:col-span-1 lg:col-span-3  h-[400px]">
+              <ScrollArea className=" md:col-span-2 sm:col-span-1 lg:col-span-3  max-h-[400px]">
                 <div className="w-full grid grid-cols-1 md:grid-cols-2  gap-2 lg:grid-cols-3 lg:col-span-3 ">
                   {categoriesChilds?.map(
                     (item: {
@@ -98,7 +118,7 @@ const Categories = () => {
                     }) => (
                       <div
                         onClick={() => onCategoryClick(item)}
-                        className="border group flex justify-between items-center h-14  border-[#E5E6E8] px-4 py-2 cursor-pointer "
+                        className="border group flex justify-between items-center h-14  border-[#f3f3f4] px-4 py-2 cursor-pointer "
                       >
                         <div className="flex gap-2">
                           <Laptop className="mr-2" />
